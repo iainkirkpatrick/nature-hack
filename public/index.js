eb9811dcce253ced7b6e261bc115b60ae1ae1cff
Jon Lemmon <jon.lemmon@enspiral.com>
2015-11-08 03:46:46 +1300

    Map that shit!!

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
 require('mapbox.js')
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 r = require('rethinkdb');
 
 console.log(process.env.IAIN_MAPBOX_ACCESS_TOKEN);
@@ -12,60 +14,90 @@ map.setView([-41.112, 172.694], 6)
 
 var pointDataLayer = L.mapbox.featureLayer().addTo(map)
 
-var sampleGeoJson = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          175.78125,
-          -38.8225909761771
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          175.166015625,
-          -39.232253141714885
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          176.00097656249997,
-          -39.36827914916012
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          175.4296875,
-          -39.67337039176559
-        ]
-      }
+fetch('/datasets/nzfapdc/Nectria')
+  .then(function(response) {
+    if (response.status >= 400) {
+      throw new Error("Bad response from server");
     }
-  ]
-}
+    return response.json();
+  })
+  .then(function(data) {
+    return {
+      "type": "FeatureCollection",
+      "features": data.map(function(d) {
+        return {
+          "type": "Feature",
+          "properties": {
+            "Name": d["Matched Scientific Name"],
+            "Date": d["Event Date - parsed"]
+          },
+          "geometry": {
+            "type": "Point",
+            "coordinates": [
+              +d["Longitude - processed"],
+              +d["Latitude - processed"]
+            ]
+          }
+        }
+      })
+    }
+  }).then(function(data){
+    var geoJson = L.geoJson(data, {
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: 5
+        })
+      }
+    }).addTo(map)
+  });
 
-var geoJson = L.geoJson(sampleGeoJson, {
-  pointToLayer: function(feature, latlng) {
-    return L.circleMarker(latlng, {
-      radius: 5
-    })
-  }
-}).addTo(map)
+
+// var sampleGeoJson = {
+//   "type": "FeatureCollection",
+//   "features": [
+//     {
+//       "type": "Feature",
+//       "properties": {},
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           175.78125,
+//           -38.8225909761771
+//         ]
+//       }
+//     },
+//     {
+//       "type": "Feature",
+//       "properties": {},
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           175.166015625,
+//           -39.232253141714885
+//         ]
+//       }
+//     },
+//     {
+//       "type": "Feature",
+//       "properties": {},
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           176.00097656249997,
+//           -39.36827914916012
+//         ]
+//       }
+//     },
+//     {
+//       "type": "Feature",
+//       "properties": {},
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           175.4296875,
+//           -39.67337039176559
+//         ]
+//       }
+//     }
+//   ]
+// }
