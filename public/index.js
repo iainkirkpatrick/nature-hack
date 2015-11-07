32be2a99cd2e5ada404fd7a42baaf9f6c3c4670c
peterjacobson <peterjacobson.nz@gmail.com>
2015-11-07 17:53:02 +1300

    scaffold mapbox & user location

--- public/index.js
+++ public/index.js
@@ -1,1 +1,30 @@
-console.log('running.');
+require('mapbox.js')
+
+console.log(process.env.IAIN_MAPBOX_ACCESS_TOKEN);
+
+var mapOptions = {
+  accessToken: 'pk.eyJ1IjoiZW52aW50YWdlIiwiYSI6Inh6U0p2bkEifQ.p6VrrwOc_w0Ij-iTj7Zz8A'
+}
+
+L.mapbox.map('map', 'envintage.i9eofp14', mapOptions)
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log('Latitude : ' + crd.latitude);
+  console.log('Longitude: ' + crd.longitude);
+  console.log('More or less ' + crd.accuracy + ' meters.');
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
